325380d3550cb8e6fe97af0fc1e5ffeca1ae2d96
vikasgaurdws <vikas.gaur@flipkart.com>
2025-01-21 17:45:03 +0530

    Rename file

---
+++ JEDI-GROUP-D-JAVA-FLIPFIT-POS/src/com/flipkart/business/FlipFitGymOperations.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.flipkart.business;
+
+/**
+ * 
+ */
+public class FlipFitGymOperations {
+	
+	public void getSlots() {
+		System.out.println("Gave slots");
+	}
+	
+	public void getWaitLists() {
+		System.out.println("Gave waitList");
+	}
+	
+	public void isVerified() {
+		System.out.println("is Verified");
+	}
+	
+	
+
+}
