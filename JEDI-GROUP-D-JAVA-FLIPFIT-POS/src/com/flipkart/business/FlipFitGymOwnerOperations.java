24dc4d0e41f57dc8dac85d49ed8fb55bd0f9a730
sandeep.s23 <sandeep.s23@fk1089-231576l.fkipl.flipkart.com>
2025-01-21 17:28:26 +0530

    Adding files

---
+++ JEDI-GROUP-D-JAVA-FLIPFIT-POS/src/com/flipkart/business/FlipFitGymOwnerOperations.java
@@ -0,0 +1,20 @@
+package com.flipkart.business;
+
+public class FlipFitGymOwnerOperations extends FlipFitUserOperations{
+    public void requestForProfileVerfication(){
+        System.out.println("requestForProfileVerfication");
+    }
+    public void requestGymVerification(){
+        System.out.println("request");
+    }
+    public void addGym(){
+        System.out.println("add");
+    }
+    public void viewGym(){
+        System.out.println("view");
+    }
+    public void isVerified(){
+        System.out.println("isVerified");
+    }
+}
+
---
+++ JEDI-GROUP-D-JAVA-FLIPFIT-POS/src/com/flipkart/business/FlipFitUserOperations.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.flipkart.business;
+
+/**
+ * 
+ */
+public class FlipFitUserOperations {
+	public void editProfile()
+	{
+		System.out.println("This method is used to edit the Profile a Customer");
+	}
+	
+	public void viewProfile(){
+     System.out.println("this fuction is to viewProfile");   
+    }
+}
