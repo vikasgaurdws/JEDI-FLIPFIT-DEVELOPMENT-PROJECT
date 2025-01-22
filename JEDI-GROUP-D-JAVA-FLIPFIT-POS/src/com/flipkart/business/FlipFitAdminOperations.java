754218d0ca62a9bab67e97545fb8afe958fc0e9e
kashishgarg1 <kashish.garg1@flipkart.com>
2025-01-22 12:48:56 +0530

    modifies admin-client

--- JEDI-GROUP-D-JAVA-FLIPFIT-POS/src/com/flipkart/business/FlipFitAdminOperations.java
+++ JEDI-GROUP-D-JAVA-FLIPFIT-POS/src/com/flipkart/business/FlipFitAdminOperations.java
@@ -1,87 +1,87 @@
 /**
- * 
+ *
  */
 package com.flipkart.business;
 
 import java.util.List;
 
 /**
- * 
+ *
  */
-public class AdminOperations{
-    
-	public void createAdmin(String UserName, int adminId)
+public class FlipFitAdminOperations{
+
+	public void createAdmin(String UserName)
 	{
 		System.out.println("Admin is Created");
 	}
-	
-	
-	public List<FlipfitGymOwner> viewAllGymOwners()
+
+
+	public void viewAllGymOwners()
 	{
 		//if() GymOwnerslist is empty
 		System.out.println("No Gym owner found!!");
 //		else
-		System.out.println("All Gym Owners\n");
-		
+		System.out.println("All Gym Owners");
+
 		// for Gymowner in GymOwners
 		System.out.println("Gym owner");
-		
+
 	}
-	
-	public List<FlipfitGyms> viewGymsDetails()
+
+	public void viewGymDetails()
 	{
 		//if() Gymslist is empty
 		System.out.println("No Gym found!!");
 //		else
 		System.out.println("All Gyms");
-		
+
 		// for Gym in Gyms
 		System.out.println("Gym");
-		
+
 	}
-	
+
 	public void approveGymOwnerRequests(int ownerId) {
 //        boolean res=adminDAO.approveGymOwnerRequests(ownerId);
 //        if (res){
-            System.out.println("Gym Owner request with ID " + ownerId + " has been approved.");
+		System.out.println("Gym Owner request with ID " + ownerId + " has been approved.");
 //        }
 //        else{
-            System.out.println("Gym Owner request with ID " + ownerId + " could not be found!!");
+		System.out.println("Gym Owner request with ID " + ownerId + " could not be found!!");
 //        }
-    }
+	}
 
-    /**
-     * Method to approve gym requests
-     * @param gymId
-     */
+	/**
+	 * Method to approve gym requests
+	 * @param gymId
+	 */
 //    @Override
-    public void approveGymRequests(int gymId) {
+	public void approveGymRequests(int gymId) {
 //        boolean res=adminDAO.approveGymRequests(gymId);
 //        if (res){
-            System.out.println("Gym request with ID " + gymId + " has been approved.");
+		System.out.println("Gym request with ID " + gymId + " has been approved.");
 //        }
 //        else{
-            System.out.println("Gym request with ID " + gymId + " could not be found!!");
+		System.out.println("Gym request with ID " + gymId + " could not be found!!");
 //        }
-    }
-    
-    public void viewPendingGymRequests()
-    {
+	}
+
+	public void viewPendingGymRequests()
+	{
 //    	GymReqList take it from DAO
 //    	if(GymReqList not empty)
-    	System.out.println("All pending Requests");
+		System.out.println("All pending Requests");
 //    	for GymReq in List print GymReq
-    }
-    public void viewPendingGymOwnerRequests()
-    {
+	}
+	public void viewPendingGymOwnerRequests()
+	{
 //    	GymOwnerReqList take it from DAO
 //    	if(GymOwnerReqList not empty)
-    	System.out.println("All pending Requests");
+		System.out.println("All pending Requests");
 //    	for GymOwnerReq in List print GymReq
-    }
-    
-    public void viewGymUsers(int gymid)
-    {
+	}
+
+	public void viewGymUsers(int gymid)
+	{
 //    	GymUserList take it from DAO
 //    	if(GymUserList not empty)
 //    	{
@@ -93,7 +93,7 @@ public class AdminOperations{
 //    	{
 //        System.out.println("Gym has no users");
 //    	}
-    }
-	
-	
+	}
+
+
 }
--- JEDI-GROUP-D-JAVA-FLIPFIT-POS/src/com/flipkart/client/FlipfitAdminClient.java
+++ JEDI-GROUP-D-JAVA-FLIPFIT-POS/src/com/flipkart/client/FlipfitAdminClient.java
@@ -1,51 +1,57 @@
 package com.flipkart.client;
 
-import com.flipkart.bean.Gym;
-import com.flipkart.bean.GymOwner;
-import com.flipkart.business.*;
+import com.flipkart.bean.FlipFitGym;
+import com.flipkart.bean.FlipFitGymOwner;
+import com.flipkart.business.FlipFitAdminOperations;
 
 import java.util.*;
 
 /**
- * 
+ *
  */
 
 public class FlipfitAdminClient {
 
-	FlipfitAdminOperations flipfitAdminOperations = new FlipfitAdminOperations();
+	FlipFitAdminOperations flipfitAdminOperations = new FlipFitAdminOperations();
 
-	List<FlipfitGymOwner> gymOwnerList;
-	List<FlipfitGym> gymList;
-	
+	List<FlipFitGymOwner> gymOwnerList;
+	List<FlipFitGym> gymList;
+
 	Scanner sc;
-	
-	FlipfitAdminClient(Scanner sc)
+
+	public FlipfitAdminClient(Scanner sc)
 	{
 		this.sc=sc;
-		gymOwnerList = flipfitAdminOperations.getGymOwners();
-		gymList = flipfitAdminOperations.getGym();
+		gymOwnerList = FlipFitAdminOperations.viewAllGymOwners();
+		gymList = FlipFitAdminOperations.viewGymDetails();
 	}
-	
-	
-	public void viewAllPendingGymOwnerRequests() {
-		viewAllGymOwners(flipfitAdminOperations.viewAllPendingGymOwnerRequests());
-	}
-	public void viewAllPendingGymRequests() {
-		viewAllGyms(flipfitAdminOperations.viewAllPendingGymRequests());
-	}
-	
-	public void handleGymOwnerRequest() {
-		System.out.println("Enter gym owner id: ");
-		flipfitAdminOperations.approveGymOwnerRequest(sc.next());
-	}
-	
-	public void handleGymRequest() {
-		System.out.println("Enter gym Id: ");
-		flipfitAdminOperations.approveGymRequest(sc.next());
+	public void createAdmin() {
+		System.out.println("Enter Admin Name: ");
+		flipfitAdminOperations.createAdmin(sc.next());
+
 	}
 
-	
-	
+	public void viewPendingGymOwnerRequests() {
+		flipfitAdminOperations.viewPendingGymOwnerRequests();
+	}
+	public void viewAPendingGymRequests() {
+		flipfitAdminOperations.viewPendingGymRequests();
+	}
+	public void approveGymOwnerRequests() {
+		System.out.println("Enter GymOwnerId: ");
+		flipfitAdminOperations.approveGymOwnerRequests(sc.next());
+	}
+	public void approveGymRequests() {
+		System.out.println("Enter Gym Id: ");
+		flipfitAdminOperations.approveGymRequests(sc.next());
+	}
+
+	public void viewGymUsers() {
+		System.out.println("Enter gym Id: ");
+		flipfitAdminOperation.viewGymUsers(sc.next());
+	}
+
+
 
 	public void adminOptions() throws Exception {
 		System.out.println("++++++++++++++++++++++++++++++++++++++++++++++");
@@ -54,40 +60,45 @@ public class FlipfitAdminClient {
 			System.out.println("2. View pending Gym Requests");
 			System.out.println("3. Approve Gym Owner Request");
 			System.out.println("4. Approve Gym Request");
+			System.out.println("4. view gym users");
+			System.out.println("4. view all gym owners");
+			System.out.println("4. view gym details");
 			System.out.println("5. Exit");
-			
+
 			System.out.print("Enter your choice: ");
 			int choice = sc.nextInt();
 			switch (choice) {
-			
-		
-			case 1:
-				viewAllPendingGymOwnerRequests();
-				break;
-				
-			case 2:
-				viewAllPendingGymRequests();
-				break;
-		
-			case 3:
-				handleGymOwnerRequest();
-				break;
-				
-			case 4:
-				handleGymRequest();
-				break;
-				
-			case 5:
-                System.out.println("Exiting...");
-                return; 	
-				
-			
-			default:
-				System.out.println("Please check the option you have entered");
+
+
+				case 1:
+					viewPendingGymOwnerRequests();
+					break;
+
+				case 2:
+					viewPendingGymRequests();
+					break;
+
+				case 3:
+					approveGymOwnerRequest();
+					break;
+
+				case 4:
+					approveGymRequest();
+					break;
+				case 5:
+					viewGymUsers;
+					break;
+				case 8:
+					System.out.println("Exiting...");
+					return;
+
+
+				default:
+					System.out.println("Please check the option you have entered");
 			}
 		}
-}
-			
-	
-	
+	}
+
+
+
 }
