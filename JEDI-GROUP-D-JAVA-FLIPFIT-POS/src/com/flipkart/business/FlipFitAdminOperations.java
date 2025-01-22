73f3f35dc9edf009bc4e0d858bb092b956d3659c
Chockalingam-A-03 <chockalingam.a@flipkart.com>
2025-01-22 12:24:04 +0530

    Update FlipFitAdminOperations.java

--- JEDI-GROUP-D-JAVA-FLIPFIT-POS/src/com/flipkart/business/FlipFitAdminOperations.java
+++ JEDI-GROUP-D-JAVA-FLIPFIT-POS/src/com/flipkart/business/FlipFitAdminOperations.java
@@ -1,15 +1,99 @@
+/**
+ * 
+ */
 package com.flipkart.business;
 
+import java.util.List;
 
-public class FlipFitAdminOperations extends FlipFitUserOperations{
-    public void requestForProfileVerfication(){
+/**
+ * 
+ */
+public class AdminOperations{
+    
+	public void createAdmin(String UserName, int adminId)
+	{
+		System.out.println("Admin is Created");
+	}
+	
+	
+	public List<FlipfitGymOwner> viewAllGymOwners()
+	{
+		//if() GymOwnerslist is empty
+		System.out.println("No Gym owner found!!");
+//		else
+		System.out.println("All Gym Owners\n");
+		
+		// for Gymowner in GymOwners
+		System.out.println("Gym owner");
+		
+	}
+	
+	public List<FlipfitGyms> viewGymsDetails()
+	{
+		//if() Gymslist is empty
+		System.out.println("No Gym found!!");
+//		else
+		System.out.println("All Gyms");
+		
+		// for Gym in Gyms
+		System.out.println("Gym");
+		
+	}
+	
+	public void approveGymOwnerRequests(int ownerId) {
+//        boolean res=adminDAO.approveGymOwnerRequests(ownerId);
+//        if (res){
+            System.out.println("Gym Owner request with ID " + ownerId + " has been approved.");
+//        }
+//        else{
+            System.out.println("Gym Owner request with ID " + ownerId + " could not be found!!");
+//        }
+    }
 
+    /**
+     * Method to approve gym requests
+     * @param gymId
+     */
+//    @Override
+    public void approveGymRequests(int gymId) {
+//        boolean res=adminDAO.approveGymRequests(gymId);
+//        if (res){
+            System.out.println("Gym request with ID " + gymId + " has been approved.");
+//        }
+//        else{
+            System.out.println("Gym request with ID " + gymId + " could not be found!!");
+//        }
     }
-    public void handleGymOwnerVerification(){
-        System.out.println("handleGymOwnerVerification");
+    
+    public void viewPendingGymRequests()
+    {
+//    	GymReqList take it from DAO
+//    	if(GymReqList not empty)
+    	System.out.println("All pending Requests");
+//    	for GymReq in List print GymReq
     }
-    public void handleGymVerfication(){
-        System.out.println("handleGymVerfication");
+    public void viewPendingGymOwnerRequests()
+    {
+//    	GymOwnerReqList take it from DAO
+//    	if(GymOwnerReqList not empty)
+    	System.out.println("All pending Requests");
+//    	for GymOwnerReq in List print GymReq
     }
+    
+    public void viewGymUsers(int gymid)
+    {
+//    	GymUserList take it from DAO
+//    	if(GymUserList not empty)
+//    	{
+//    		User id list = GymUserList[gymid]
+//    	    for User in Userlist:
+//    	    System.out.println("User details")
+//    	}
+//    	else
+//    	{
+//        System.out.println("Gym has no users");
+//    	}
+    }
+	
+	
 }
-
