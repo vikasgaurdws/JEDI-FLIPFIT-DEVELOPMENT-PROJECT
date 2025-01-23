9cac57066b930b240e12fe80f43b631e35372b73
venkatpt7 <venkat.p@flipkart.com>
2025-01-23 14:11:33 +0530

    utils intial commit

---
+++ JEDI-GROUP-D-JAVA-FLIPFIT-POS/src/com/flipkart/utils/DBconnection.java
@@ -0,0 +1,43 @@
+package com.flipkart.utils;
+
+import java.sql.*;
+
+public class DBconnection {
+	private static Connection connection = null;
+	private static String url = "jdbc:mysql://localhost:3306/flipfit_Schema";
+	private static String username = "root";
+	private static String password = "teja@2004";
+
+	
+	public static Connection getConnection() {
+		
+        if (connection != null)
+            return connection;
+        else {
+        	
+        	 try {
+
+ 	        	Class.forName("com.mysql.cj.jdbc.Driver");
+
+ 	        	connection = DriverManager.getConnection(url, username, password);
+ 	            System.out.println("Connection successful!");
+
+ 	           } catch (ClassNotFoundException e) {
+ 	            System.err.println("MySQL JDBC Driver not found.");
+ 	            e.printStackTrace();
+ 	        } catch (SQLException e) {
+ 	            System.err.println("Connection failed!");
+ 	            e.printStackTrace();
+ 	        } 
+            
+        }
+        
+        return connection;
+        
+        
+        }
+	
+            
+ }
+
+
