5036ae3b1628a02f310eac23aababd36a777554b
Chockalingam-A-03 <chockalingam.a@flipkart.com>
2025-01-24 11:43:39 +0530

    Create FlipFitGymOwnerDAOImpl.java

---
+++ JEDI-GROUP-D-JAVA-FLIPFIT-POS/src/com/flipkart/DAO/FlipFitGymOwnerDAOImpl.java
@@ -0,0 +1,52 @@
+package com.flipkart.DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.flipkart.bean.*;
+import com.flipkart.utils.DBconnection;
+
+public class FlipFitGymOwnerDAOImpl implements FlipFitGymOwnerDAO{
+
+	@Override
+	public FlipFitGymOwner getFlipFitGymOwnerDetails(String FlipFitGymOwnerEmailId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void editFlipFitGymOwnerDetails(String FlipFitGymOwnerEmailId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addGym(String FlipFitGymOwnerEmailId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void editGym(String FlipFitGymOwnerEmailId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<FlipFitGym> getGymsOfFlipFitGymOwner(String FlipFitGymOwnerEmailId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addSlot() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
